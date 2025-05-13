6fd30ce63c2a29fa33937d947f2b27aeb9669d7f
lokesh <lokesh@MacBook-Air-2.local>
2025-05-13 22:59:38 +0530

    pydantic updated

---
+++ chatbot/app.py
@@ -0,0 +1,12 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import streamlit as st
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+st.title("LangChain Demo with OPENAI API")
+
